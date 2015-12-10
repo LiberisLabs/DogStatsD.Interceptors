c52f62c40303a5c8f92584283f68a4f8d500b1fa
kev_bite <kev_bite@msn.com>
2015-12-10 11:40:20 +0000

    Pushing in a IDogStatD to the DataDogInstrumentMonitor

--- src/LiberisLabs.DogStatsD.Interceptors/MonitorInterceptorFactory.cs
+++ src/LiberisLabs.DogStatsD.Interceptors/MonitorInterceptorFactory.cs
@@ -6,13 +6,20 @@ namespace LiberisLabs.DogStatsD.Interceptors
 {
     public class MonitorInterceptorFactory
     {
+        private readonly IDogStatsd _dogStatsd;
+
+        public MonitorInterceptorFactory()
+        {
+            _dogStatsd = new DogStatsdWrapper();
+        }
+
         public ICollection<IMonitor> CreateMonitorInterceptors(MethodInfo method, MethodInfo methodInvocationTarget)
         {
             var monitorInterceptor = new List<IMonitor>();
 
             if (method.HasAttribute<InstrumentAttribute>() || methodInvocationTarget.HasAttribute<InstrumentAttribute>())
             {
-                monitorInterceptor.Add(new DataDogInstrumentMonitor(methodInvocationTarget));
+                monitorInterceptor.Add(new DataDogInstrumentMonitor(methodInvocationTarget, _dogStatsd));
             }
 
             return monitorInterceptor;
