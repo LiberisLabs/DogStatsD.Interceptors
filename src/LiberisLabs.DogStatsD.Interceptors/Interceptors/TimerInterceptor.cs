e75068237dd08c8bf6e74f63bba5b714c0223489
kev_bite <kev_bite@msn.com>
2015-12-16 17:17:56 +0000

    Tests for CanIntercept for timer.

--- src/LiberisLabs.DogStatsD.Interceptors/Interceptors/TimerInterceptor.cs
+++ src/LiberisLabs.DogStatsD.Interceptors/Interceptors/TimerInterceptor.cs
@@ -36,8 +36,8 @@ namespace LiberisLabs.DogStatsD.Interceptors.Interceptors
 
         public bool CanIntercept(MethodInfo methodInfo, MethodInfo methodInvocationTarget)
         {
-            return (methodInfo.HasAttribute<TimeAttribute>() || methodInvocationTarget.HasAttribute<TimeAttribute>())
-                   && !typeof (Task).IsAssignableFrom(methodInfo.ReturnType);
+            return !typeof (Task).IsAssignableFrom(methodInfo.ReturnType)
+                   && (methodInfo.HasAttribute<TimeAttribute>() || methodInvocationTarget.HasAttribute<TimeAttribute>());
         }
     }
 }
