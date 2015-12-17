85638d7446faaaa2e715e65a480c9c871fe409fb
kev_bite <kev_bite@msn.com>
2015-12-17 14:18:31 +0000

    tests around CanMonitor.

--- src/LiberisLabs.DogStatsD.Interceptors.Tests/Monitors/InstrumentMonitorTests.cs
+++ src/LiberisLabs.DogStatsD.Interceptors.Tests/Monitors/InstrumentMonitorTests.cs
@@ -1,4 +1,6 @@
-﻿using LiberisLabs.DogStatsD.Interceptors.Monitors;
+﻿using System.Reflection;
+using LiberisLabs.DogStatsD.Interceptors.Annotations;
+using LiberisLabs.DogStatsD.Interceptors.Monitors;
 using LiberisLabs.DogStatsD.Interceptors.Tests.Helpers;
 using Moq;
 using NUnit.Framework;
@@ -51,6 +53,62 @@ namespace LiberisLabs.DogStatsD.Interceptors.Tests.Monitors
             _monitor.Success();
 
             _dogStatsd.Verify(x => x.Increment("namespace.classname.methodname.success", 1, 1D, null), Times.Once);
+        }
+
+        [Test]
+        public void WhenCallingCanMonitorWithNoAttibutesOn_ReturnsFalse()
+        {
+            var methodInfo = new Mock<MethodInfo>();
+            methodInfo.Setup(x => x.GetCustomAttributes(typeof(InstrumentAttribute), false))
+                .Returns(new InstrumentAttribute[0]);
+
+            var result = _monitor.CanMonitor(methodInfo.Object, methodInfo.Object);
+
+            Assert.That(result, Is.False);
+        }
+        
+        [Test]
+        public void WhenCallingCanMonitorWithAttibutesOnMethodInfo_ReturnsTrue()
+        {
+            var methodInfo = new Mock<MethodInfo>();
+            methodInfo.Setup(x => x.GetCustomAttributes(typeof (InstrumentAttribute), false))
+                .Returns(new[] {new InstrumentAttribute()});
+
+            var methodInvocationTarget = new Mock<MethodInfo>();
+            methodInvocationTarget.Setup(x => x.GetCustomAttributes(typeof(InstrumentAttribute), false))
+                .Returns(new InstrumentAttribute[0]);
+
+            var result = _monitor.CanMonitor(methodInfo.Object, methodInvocationTarget.Object);
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void WhenCallingCanMonitorWithAttibutesOnMethodInvocationTarget_ReturnsTrue()
+        {
+            var methodInfo = new Mock<MethodInfo>();
+            methodInfo.Setup(x => x.GetCustomAttributes(typeof(InstrumentAttribute), false))
+                .Returns(new InstrumentAttribute[0]);
+
+            var methodInvocationTarget = new Mock<MethodInfo>();
+            methodInvocationTarget.Setup(x => x.GetCustomAttributes(typeof(InstrumentAttribute), false))
+                .Returns(new[] { new InstrumentAttribute() });
+
+            var result = _monitor.CanMonitor(methodInfo.Object, methodInvocationTarget.Object);
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void WhenCallingCanMonitorWithAttibutesBothMethodInfo_ReturnsTrue()
+        {
+            var methodInfo = new Mock<MethodInfo>();
+            methodInfo.Setup(x => x.GetCustomAttributes(typeof(InstrumentAttribute), false))
+                .Returns(new[] { new InstrumentAttribute() });
+
+            var result = _monitor.CanMonitor(methodInfo.Object, methodInfo.Object);
+
+            Assert.That(result, Is.True);
         }
     }
 }
